6d69da157832a9b2250e112458963d59d359efff
Pawel Tryniecki <paweltry303@gmail.com>
2024-06-06 22:48:49 +0200

    Create Card.java

---
+++ Blackjack/Card.java
@@ -0,0 +1,15 @@
+package Blackjack;
+
+public class Card{
+  public String name;
+  public int wert;
+  public void drive() {
+    System.out.println("heheheha");
+  }
+  public static void gewonnen(){
+    System.out.println("Du hast gewonnen");
+  }
+  public static void main(String[] args) {
+    gewonnen();
+}
+}
